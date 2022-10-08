626ab76e2724f93241d0e9a8dda97c381efa30dc
MIRZOULUGBEK9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2022-10-08 18:46:55 +0500

    Refactor .language

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
+// MODIFIERS
+const modifiers = {
+  toggle: "language__toggler--open",
+  list: "language__list--open"
+}
+// VARIABLES
 const elLoader = document.querySelector(".js-loader"),
 elLangToggler = document.querySelector(".js-lang-toggler"),
-elLangList = document.querySelector(".js-lang-list");
-
+elLangList = document.querySelector(".js-lang-list"),
+elLangLink = elLangList.querySelectorAll(".language__link");
+// LOADER
 document.addEventListener("DOMContentLoaded", function () {
   setTimeout(function (){
     elLoader.classList.add("loader-wrapper--none")
@@ -10,7 +17,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
 if (elLangToggler) {
   elLangToggler.addEventListener("click", function (){
-    elLangToggler.classList.toggle("language__toggler--open");
-    elLangList.classList.toggle("language__list--open")
+    elLangToggler.classList.toggle(modifiers.toggle);
+    elLangList.classList.toggle(modifiers.list);
   });
 }
+
+elLangLink.forEach(function (link, index){
+  link.addEventListener("click", function () {
+    elLangToggler.textContent = link.textContent;
+    elLangToggler.classList.toggle(modifiers.toggle);
+    elLangList.classList.toggle(modifiers.list);
+  });
+});
