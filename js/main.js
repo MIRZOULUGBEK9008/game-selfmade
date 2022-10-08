e971883510c5663148c7d167fe0cc0055ea7f64b
MIRZOULUGBEK9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2022-10-08 23:37:34 +0500

    Add responsibility to site-header and hero (1000px)

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
 // MODIFIERS
 const modifiers = {
   toggle: "language__toggler--open",
-  list: "language__list--open"
+  list: "language__list--open",
+  nav: "site-header__sitenav--open",
+  transparent: "navbar--transparent-one",
+  overflow: "body-overflow"
 }
 // VARIABLES
 const elLoader = document.querySelector(".js-loader"),
 elLangToggler = document.querySelector(".js-lang-toggler"),
 elLangList = document.querySelector(".js-lang-list"),
-elLangLink = elLangList.querySelectorAll(".language__link");
+elLangLink = elLangList.querySelectorAll(".language__link"),
+elNav = document.querySelector(".js-nav"),
+elNavbar = document.querySelectorAll(".js-navbar"),
+elNavbarTrasparent = document.querySelector(".js-navbar-transparent"),
+elNavLink = elNav.querySelectorAll(".sitenav__link");
 // LOADER
 document.addEventListener("DOMContentLoaded", function () {
   setTimeout(function (){
@@ -27,5 +34,21 @@ elLangLink.forEach(function (link, index){
     elLangToggler.textContent = link.textContent;
     elLangToggler.classList.toggle(modifiers.toggle);
     elLangList.classList.toggle(modifiers.list);
+  });
+});
+
+elNavbar.forEach(function (navbar, index){
+  navbar.addEventListener("click", function () {
+    elNav.classList.toggle(modifiers.nav);
+    elNavbarTrasparent.classList.toggle(modifiers.transparent);
+    document.body.classList.toggle(modifiers.overflow);
+  });
+});
+
+elNavLink.forEach(function (navlink, index){
+  navlink.addEventListener("click", function (){
+    elNav.classList.toggle(modifiers.nav);
+    elNavbarTrasparent.classList.toggle(modifiers.transparent);
+    document.body.classList.toggle(modifiers.overflow);
   });
 });
