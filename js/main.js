c0c3d6d84dc3c1ed5a9f705f6de325e7e990041c
MIRZOULUGBEK9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2022-10-08 17:44:15 +0500

    Add site-header and hero (.js .html .css)

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
-const elLoader = document.querySelector(".js-loader");
+const elLoader = document.querySelector(".js-loader"),
+elLangToggler = document.querySelector(".js-lang-toggler"),
+elLangList = document.querySelector(".js-lang-list");
 
 document.addEventListener("DOMContentLoaded", function () {
   setTimeout(function (){
     elLoader.classList.add("loader-wrapper--none")
   }, 800);
 });
+
+if (elLangToggler) {
+  elLangToggler.addEventListener("click", function (){
+    elLangToggler.classList.toggle("language__toggler--open");
+    elLangList.classList.toggle("language__list--open")
+  });
+}
